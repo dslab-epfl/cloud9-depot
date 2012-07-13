62c05c8a850367a8229887b27e251c12647cfb4a
Stefan Bucur <stefan.bucur@epfl.ch>
2012-07-13 18:22:01 +0200

    Updated the bundling script to pick up Boolector.

--- tools/gce/bundle_cloud9.sh
+++ tools/gce/bundle_cloud9.sh
@@ -7,6 +7,7 @@ THIS_DIR="$(dirname "${0}")"
 CLOUD9_DIR="${THIS_DIR}/.."
 
 CLOUD9_BUILD_DIR="${CLOUD9_DIR}/cloud9/Release+Asserts"
+DEPOT_BUILD_DIR="${CLOUD9_DIR}/out/Default"
 LLVM_BUILD_DIR="${CLOUD9_DIR}/third_party/llvm-build/Release+Asserts"
 TARGETS_BUILD_DIR="${CLOUD9_DIR}/testing_targets/out/Default"
 UCLIBC_BUILD_DIR="${CLOUD9_DIR}/klee-uclibc"
@@ -27,6 +28,8 @@ mkdir "${ARCHIVE_DIR}/cloud9/llvm-bin"
 
 cp -r ${CLOUD9_BUILD_DIR}/bin/* "${ARCHIVE_DIR}/cloud9/bin"
 cp -r ${CLOUD9_BUILD_DIR}/lib/* "${ARCHIVE_DIR}/cloud9/lib"
+
+cp ${DEPOT_BUILD_DIR}/lib.target/* "${ARCHIVE_DIR}/cloud9/lib"
 
 # TODO(bucur): Enable this via option
 #cp -r ${LLVM_BUILD_DIR}/bin/* "${ARCHIVE_DIR}/cloud9/bin"
